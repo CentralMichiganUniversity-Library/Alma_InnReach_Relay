5de36a6c1e50184112e504af9d54befa0f0fa146
Eric Cronstrom <crons1eg@cmich.edu>
2017-12-14 15:09:38 -0500

    Adding testing script

---
+++ Testing/Test.ps1
@@ -0,0 +1,4 @@
+﻿$body = Get-Content .\LookupUser.xml
+$relayAddress = 'http://localhost:59886/NCIPRelay'
+
+Invoke-WebRequest -Uri $relayAddress -Method Post -Body $body | Select-Object -ExpandProperty Content
